8309cdd2edc896e61c31d7f0e7e9b3785c431203
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-07-20 16:59:31 -0700

    main page seasonal and who we are styles add

--- js/index.js
+++ js/index.js
@@ -51,3 +51,79 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+
+document.addEventListener("DOMContentLoaded", function() {
+  const aboutCards = document.querySelectorAll("[class^='about__card-']");
+  
+  // Function to apply grayscale to other cards when hovering over any card
+  function applyGrayscale(event) {
+    const hoveredCard = event.target;
+    for (const card of aboutCards) {
+      if (card !== hoveredCard) {
+        card.classList.add("grayscale");
+      }
+    }
+    hoveredCard.classList.add("scale");
+  }
+
+  // Function to remove grayscale and scaling when not hovering over any card
+  function removeEffects() {
+    for (const card of aboutCards) {
+      card.classList.remove("grayscale");
+      card.classList.remove("scale");
+    }
+  }
+
+  // Event listeners for hover in and out on all cards
+  for (const card of aboutCards) {
+    card.addEventListener("mouseenter", applyGrayscale);
+    card.addEventListener("mouseleave", removeEffects);
+  }
+});
+
+
+
+
+
+
+
+const seasonCardsFood = document.querySelectorAll(".season [class*='--food_pic']");
+const seasonCardsWine = document.querySelectorAll(".season [class*='--wine_pic']");
+
+function showFoodPrice(event) {
+  const card = event.currentTarget;
+  card.classList.add('grayscale2');
+  const priceElement = card.querySelector('.f-price');
+  priceElement.classList.remove('f-hidden');
+}
+
+function hideFoodPrice(event) {
+  const card = event.currentTarget;
+  card.classList.remove('grayscale2');
+  const priceElement = card.querySelector('.f-price');
+  priceElement.classList.add('f-hidden');
+}
+
+function showWinePrice(event) {
+  const card = event.currentTarget;
+  card.classList.add('grayscale2');
+  const priceElement = card.querySelector('.w-price');
+  priceElement.classList.remove('w-hidden');
+}
+
+function hideWinePrice(event) {
+  const card = event.currentTarget;
+  card.classList.remove('grayscale2');
+  const priceElement = card.querySelector('.w-price');
+  priceElement.classList.add('w-hidden');
+}
+
+seasonCardsFood.forEach((card) => {
+  card.addEventListener("mouseenter", showFoodPrice);
+  card.addEventListener("mouseleave", hideFoodPrice);
+});
+
+seasonCardsWine.forEach((card) => {
+  card.addEventListener("mouseenter", showWinePrice);
+  card.addEventListener("mouseleave", hideWinePrice);
+});
